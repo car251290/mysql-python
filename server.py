ac8d18f1488c1a6eb9cf8f47dca40a1ad27db97c
Javier  Carreno <35305955+car251290@users.noreply.github.com>
2024-03-25 16:40:39 -0700

    Add files via upload

---
+++ server.py
@@ -0,0 +1,25 @@
+#`https://www.freecodecamp.org/news/connect-python-with-sql/`
+
+import mysql.connector
+from mysql.connector import Error
+import pandas as pd
+
+
+def create_server_connection(host_name,user_name,user_password):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host = host_name,
+            user = user_name,
+            password = user_password
+        )
+        print("MySQL Database connection succesful")
+    except Error as err:
+        print(f"Error: '{err}'")
+        
+    return connection
+
+
+create_server_connection()
+
+
